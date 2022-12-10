d888410711a96698872bb9e55d6da23c92ea2afd
tuyndev <tuyenpn@vtconline.com>
2022-12-10 15:24:00 +0700

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const CURRENT_TAB = "mung_tan_thu";
+let CURRENT_TAB = "mung_tan_thu";
 
 const TAB_LIST = {
   mung_tan_thu: "./assets/images/tab_mung_tan_thu.png",
@@ -7,35 +7,51 @@ const TAB_LIST = {
 };
 
 $(document).ready(function () {
+  
+  //reload lại page khi xoay ngang màn hình (load lại css)
+  $(window).on("orientationchange", function () {
+    window.location.href = window.location.href;
+  });
+
+  function renderCurrentTab(currentTab) {
+    $("#current_tab").attr("src", currentTab);
+  }
+  renderCurrentTab(TAB_LIST["qua_dap_le"]);
+
+  $("#tab_mungtanthu").click(function () {
+    CURRENT_TAB = "mung_tan_thu";
+    renderCurrentTab(TAB_LIST["mung_tan_thu"]);
+  });
+
+  $("#tab_daple").click(function () {
+    CURRENT_TAB = "qua_dap_le";
+    renderCurrentTab(TAB_LIST["qua_dap_le"]);
+  });
+
+  $("#tab_vucongvip").click(function () {
+    CURRENT_TAB = "qua_vu_cong_vip";
+    renderCurrentTab(TAB_LIST["qua_vu_cong_vip"]);
+  });
+
+  $("#btn_receive_day1").click(function () {
+    alert(`Nhận quà ${CURRENT_TAB} ngày 1`);
+  });
+
+  $("#btn_receive_day2").click(function () {
+    alert(`Nhận quà ${CURRENT_TAB} ngày 2`);
+  });
+
+  $("#btn_receive_day3").click(function () {
+    alert(`Nhận quà ${CURRENT_TAB} ngày 3`);
+  });
+
   $("#menu_mobile").click(function () {
-    $("#overlay_bg").addClass("overlay_bg");
-    $("#menu_header").addClass("menu-header__active");
+    $("#overlay_bg").toggleClass("overlay_bg");
+    $("#menu_header").toggleClass("menu-header__active");
   });
 
   $("#overlay_bg").click(function () {
     $("#overlay_bg").removeClass("overlay_bg");
     $("#menu_header").removeClass("menu-header__active");
-  });
-  
-//reload lại page khi xoay ngang màn hình
-  $(window).on("orientationchange", function () {
-    window.location.href = window.location.href;
-  });
-
-  function renderTabList(currentTab) {
-    $("#current_tab").attr("src", currentTab);
-  }
-  renderTabList(TAB_LIST["mung_tan_thu"]);
-
-  $("#tab_mungtanthu").click(function () {
-    renderTabList(TAB_LIST["mung_tan_thu"]);
-  });
-
-  $("#tab_daple").click(function () {
-    renderTabList(TAB_LIST["qua_dap_le"]);
-  });
-
-  $("#tab_vucongvip").click(function () {
-    renderTabList(TAB_LIST["qua_vu_cong_vip"]);
   });
 });
